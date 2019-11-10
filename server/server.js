0f88ab457259af3b66a8f0edfa5dc20c4f7e3192
Iris Noh <irisnoh22@gmail.com>
2019-11-09 21:16:50 -0600

    GET movies on the DOM

--- server/server.js
+++ server/server.js
@@ -3,15 +3,25 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const movieRouter = require('./routes/movie.router');
+const pool = require('./modules/pool');
+
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-app.use('/movie', movieRouter);
+// app.use('/movie', movieRouter);
 
-
+app.get('/movie', (req, res) => {
+    const queryText = 'SELECT "title" FROM "movies"';
+    pool.query(queryText)
+      .then((result) => { res.send(result.rows); })
+      .catch((err) => {
+        console.log('Error completing SELECT movie query', err);
+        res.sendStatus(500);
+      });
+  });
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
     console.log('Listening on port: ', port);
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { HashRouter as Link, Route, Router} from 'react-router-dom';
 import { connect } from 'react-redux';
 import MovieDetails from '../MovieDetails/MovieDetails'
+import { withRouter } from 'react-router-dom';
 
 class Movie extends Component {
   // Renders the entire app on the DOM
-
+componentDidMount() {
+    this.getMovie()
+}
   getMovie() {
     this.props.dispatch({ type: 'GET_MOVIE' })
 }
@@ -13,17 +16,17 @@ class Movie extends Component {
     return (
       <>
     <div>Movie Page:</div>
-    {/* {this.props.reduxState.movies.map((movie, i) => {
-                        return (<li key={i}>{movie.name}  */}
+    {this.props.reduxState.movies.map((movie, i) => {
+                        return (<li key={i}>{movie.title} 
                         {/* <DeleteForeverIcon color='secondary' size='small' variant='outlined' 
                             onClick={()=>this.deletePlant(plant.id)}>DELETE
                         </DeleteForeverIcon>
                         <LocalFloristSharpIcon color="secondary" fontSize="small" onClick={()=>this.handlePlantClick(plant)}/> */}
-                        {/* </li>)
-                    })} */}
+                        </li>)
+                    })}
 
  <pre>
-          {JSON.stringify(this.props.reduxState, null, 2)}
+          {JSON.stringify(this.props.reduxState.movies, null, 2)}
         </pre>
      </>
     );
@@ -34,7 +37,7 @@ const mapReduxStateToProps = reduxState => ({
 });
 
 
-export default connect(mapReduxStateToProps)(Movie);
+export default withRouter(connect(mapReduxStateToProps)(Movie));
 
 
 
